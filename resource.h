3bdd9f0534db535373c5a79912a64d59afa53e48
HajerChetoui <hajerchetoui@gmail.com>
2026-09-07 14:45:19 +0800

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,14 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+#define IDI_ICON1                       101
+#define IDR_FONT_DATA                   201
+#define IDR_ICON_PNG_DATA               202
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        203
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
